bc0f1425e808fa20347250c971c9c8bd2b45e3f8
roble111 <struckgold888@gmail.com>
2025-05-01 00:11:29 +0000

    Finished Domain Name Project

--- src/app.js
+++ src/app.js
@@ -6,6 +6,24 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+  let pronoun = ['the', 'our'];
+  let adj = ['great', 'big'];
+  let noun = ['jogger', 'racoon'];
   console.log("Hello Rigo from the console!");
+
+  const domains = document.querySelector("#domains");
+  for(let i=0; i < pronoun.length; i++){
+    for(let j=0; j< adj.length; j++){
+      for(let k=0; k< noun.length; k++){
+        const domainName = pronoun[i] + adj[j] + noun[k]
+        console.log(domainName)
+        const listElement=document.createElement("li")
+        listElement.innerHTML = domainName
+        domains.appendChild(listElement)
+      }
+    }
+    
+    
+  }
+
 };
